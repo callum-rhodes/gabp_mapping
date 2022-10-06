2190fcccafed64ea2e35a680164001424e763407
Callum Rhodes <92303888+callum-rhodes@users.noreply.github.com>
2022-10-06 16:35:20 +0100

    Add files via upload
    Comments added

--- scripts/main_2D.py
+++ scripts/main_2D.py
@@ -2,33 +2,32 @@
 
 from gabp_mapping import maps, GaBP, sensors
 import rospy
+import os
 from std_msgs.msg import Float32
 from nav_msgs.msg import OccupancyGrid
 import threading
+import numpy as np
 
 
 def init_ros():
     rospy.init_node('gabp', anonymous=True)
+    #  Parameter for defining the filepath to save .npy files
+    output_path = rospy.get_param('~output_path', '{}/'.format(os.environ['HOME']))
+    #  rospy.loginfo('Saving data to filepath: {}'.format(output_path))
 
-    #  rospy.set_param('~pose_topic', '/mavros/local_position/pose')
-    pose_topic = rospy.get_param('~pose_topic', '/pose')
-    rospy.loginfo('Using pose topic: {}'.format(pose_topic))
-
-    #  rospy.set_param('~sensor_topic', '/sensorLow/integrated,/sensorHigh/integrated')
-    #  rospy.set_param('~sensor_topic', '/PID')
     sensor_topic = rospy.get_param('~sensor_topic', '/gasSensor')
     rospy.loginfo('Using sensor topic: {}'.format(sensor_topic))
 
-    #  rospy.set_param('~map_frame', '/husky/map')
-    map_frame = rospy.get_param('~map_frame', '/map')
+    sensor_freq = rospy.get_param('~sensor_freq', '0')
+    sensor_freq = float(sensor_freq)
+    rospy.loginfo('Expected sensor frequency: {}'.format(sensor_freq))
+
+    map_frame = rospy.get_param('~map_frame', 'map')
     rospy.loginfo('Using map frame: {}'.format(map_frame))
 
-    #  rospy.set_param('~sensor_child_frame', '/sensorLow,/sensorHigh')
-    #  rospy.set_param('~sensor_child_frame', '/pose')
-    sensor_child_frame = rospy.get_param('~sensor_child_frame', '/base_link')
+    sensor_child_frame = rospy.get_param('~sensor_child_frame', 'base_link')
     rospy.loginfo('Using sensor child frame: {}'.format(sensor_child_frame))
 
-    #  rospy.set_param('~occ_map_topic', '/projected_map')
     map_topic = rospy.get_param('~occ_map_topic', '/map')
     rospy.loginfo('Using map topic: {}'.format(map_topic))
 
@@ -40,15 +39,16 @@ def init_ros():
     freq_val = float(freq_val)
     rospy.loginfo('Topic publish frequency: {}Hz'.format(freq_val))
 
-    #  rospy.set_param('~use_blank_map', 'False')
     use_blank_map = rospy.get_param('~use_blank_map', 'False')
     rospy.loginfo('Using blank map?: {}'.format(use_blank_map))
 
-    if use_blank_map == 'True':
+    if use_blank_map:
+        print('IT WORKED')
         rospy.set_param('~occ_freq', 1e-9)
         map_size = rospy.get_param('~map_size', '50,50')
         rospy.loginfo('Setting blank map with size ' + map_size)
     else:
+        print('IT DIDNT WORK')
         map_size = []
 
     oct_update_freq = rospy.get_param('~occ_freq', 0.1)
@@ -65,8 +65,6 @@ def init_ros():
     d_val = float(d_val)
     eps_val = rospy.get_param('~propagation_epsilon', 1)
     eps_val = float(eps_val)
-    #  rospy.set_param('~distance_metric', 'euclidean')
-    #  rospy.set_param('~distance_metric', 'mahalanobis')
     metric = rospy.get_param('~distance_metric', 'bhattacharyya')
     rospy.loginfo('Prior precision: %f', p_val)
     rospy.loginfo('Observation precision: %f', o_val)
@@ -75,24 +73,24 @@ def init_ros():
     rospy.loginfo('Propagation threshold: %f', eps_val)
     rospy.loginfo('Distance metric: ' + metric)
 
-    return pose_topic, sensor_topic, map_frame, sensor_child_frame, map_topic, resolution, freq_val, oct_update_freq, p_val, o_val, t_val, d_val, eps_val, metric, use_blank_map, map_size
+    return output_path, sensor_topic, sensor_freq, map_frame, sensor_child_frame, map_topic, resolution, freq_val, oct_update_freq, p_val, o_val, t_val, d_val, eps_val, metric, use_blank_map, map_size
 
 
 if __name__ == '__main__':
     try:
-        [pose_topic, sensor_topic, map_frame, sensor_child_frame, map_topic, resolution, freq_val, occ_update_freq, p_val, o_val, t_val, d_val, eps_val, metric, blank_bool, map_size] = init_ros()
+        [output_path, sensor_topic, sensor_freq, map_frame, sensor_child_frame, map_topic, resolution, freq_val, occ_update_freq, p_val, o_val, t_val, d_val, eps_val, metric, blank_bool, map_size] = init_ros()
         sensor_msg_type = Float32
         occ = maps.OccGenerator()
-        if blank_bool == 'True':
-            map_size = [int(float(x) / resolution) for x in map_size.split(",")]
+        if blank_bool: # If using the blank map boolean initiates based on the requested size at [0,0]
+            map_size = [int(float(x) / resolution) for x in map_size.split(",")] 
             occ.blank2occ(map_size, resolution)
-        else:
+        else: #  Else will read the occupancy grid message for the map
             occ_msg = rospy.wait_for_message(map_topic, OccupancyGrid)
-            occ.ros2occ(occ_msg, resolution)
+            occ.ros2occ(occ_msg, resolution) #  Converts the occupancy grid msg into a 2D matrix of binary values
 
         ################ DEFINE GAS SENSORS ###################
-        sensor_topic_list = sensor_topic.split(",")
-        child_frame_list = sensor_child_frame.split(",")
+        sensor_topic_list = sensor_topic.split(",") #  Extracts the sensor topics
+        child_frame_list = sensor_child_frame.split(",") #  Extracts the sensor frame names
         gas_sensor = []
         meas_idx = []
         for sens_num in range(0, len(sensor_topic_list)):
@@ -103,22 +101,25 @@ if __name__ == '__main__':
 
         ################ INITIALISE GABP OBJECT ###################
         tic = rospy.get_rostime()
-        GaBP_k = GaBP.G_GaBP_2D(occ, map_frame, lambda_p=p_val, lambda_o=o_val, lambda_d=d_val, epsilon=eps_val, lambda_t=t_val, pub_freq=freq_val)
+        GaBP_k = GaBP.G_GaBP_2D(occ, map_frame, lambda_p=p_val, lambda_o=o_val, lambda_d=d_val, epsilon=eps_val, lambda_t=t_val, pub_freq=freq_val, sens_freq=sensor_freq, filepath=output_path)
         toc = rospy.get_rostime()
         rospy.loginfo('------ GaBP initialised ------')
         rospy.loginfo("[Factor Graph Init]: %.3fs", toc.secs - tic.secs + 1e-9 * (toc.nsecs - tic.nsecs))
 
-        resid_loop = threading.Thread(target=GaBP_k.residual_loop)  # Start residual looping
+        resid_loop = threading.Thread(target=GaBP_k.residual_loop)  # Start residual propagation in another thread for when not performing wildfire
         resid_loop.start()
 
         mapLoop_start = rospy.get_time()
+	rospy.on_shutdown(GaBP_k.on_shutdown)
+	occ_itr = 0
+        sim_start = rospy.get_time()
 
         while not rospy.is_shutdown():
             ################ CHECKING FOR NEW MEASUREMENTS ###################
             for sens_num in range(0, len(sensor_topic_list)):
                 data_len = len(gas_sensor[sens_num].data)
-                if data_len > meas_idx[sens_num]:
-                    for data_idx in range(meas_idx[sens_num], data_len):
+                if data_len > meas_idx[sens_num]: # If more measurements in sensor object than in inserted into GaBP
+                    for data_idx in range(meas_idx[sens_num], data_len): # Adds only the new measurements into the dummy sensor
                         add_measures.data.append(gas_sensor[sens_num].data[data_idx])
                         add_measures.pose.append(gas_sensor[sens_num].pose[data_idx])
                         add_measures.timestamp.append(gas_sensor[sens_num].timestamp[data_idx])
@@ -127,25 +128,34 @@ if __name__ == '__main__':
             ################ ADD NEW MEASUREMENTS TO GABP ###################
             if add_measures.data:
                 tic = rospy.get_rostime()
-                GaBP_k.add_obs(add_measures)
+                GaBP_k.add_obs(add_measures)  #  inserts new sensors measurements into solver
                 toc = rospy.get_rostime()
-                rospy.loginfo("[Wildfire]:%d measurements in %.2fs", len(add_measures.data),  toc.secs - tic.secs + 1e-9 * (toc.nsecs - tic.nsecs))
-                add_measures.data = []
+                rospy.loginfo("[Wildfire]:%d measurements in %.3fs", len(add_measures.data),  toc.secs - tic.secs + 1e-9 * (toc.nsecs - tic.nsecs))
+                add_measures.data = [] # resets the dummy sensor to blank for the next set of measurements
                 add_measures.pose = []
                 add_measures.timestamp = []
 
             if rospy.get_time() - mapLoop_start > 1 / occ_update_freq:
+                ## Uncomment if you need to save conc/var maps to file
+		#  np.save(output_path + 'GaBP_mean_matrix'+ str(occ_itr) +'.npy', GaBP_k.mean)
+                #  rospy.loginfo('mean map saved to: ' + output_path + 'GaBP_mean_matrix'+ str(occ_itr) +'.npy')
+                #  np.save(output_path + 'GaBP_var_matrix'+ str(occ_itr) +'.npy', GaBP_k.var)
+                #  rospy.loginfo('variance map saved to: ' + output_path + 'GaBP_var_matrix'+ str(occ_itr) +'.npy')
+                #  np.save(output_path + 'GaBP_meta_data'+ str(occ_itr) +'.npy', np.array([GaBP_k.origin, GaBP_k.gridsize, GaBP_k.N, len(GaBP_k.z_pos),GaBP_k.msgs_wild, GaBP_k.msgs_resid, rospy.get_time() - sim_start]))
+                #  rospy.loginfo('meta data saved to: ' + output_path + 'GaBP_meta_data'+ str(occ_itr) +'.npy')
+		occ_itr += 1
                 rospy.loginfo("+++++ UPDATING OCCUPANCY MAP +++++")
                 tic = rospy.get_rostime()
                 occ = maps.OccGenerator()
                 occ_msg = rospy.wait_for_message(map_topic, OccupancyGrid)
                 occ.ros2occ(occ_msg, resolution)
                 toc = rospy.get_rostime()
-                rospy.loginfo("[Occupancy grid]: converted in %.2fs", toc.secs - tic.secs + 1e-9 * (toc.nsecs - tic.nsecs))
+                rospy.loginfo("[Occupancy grid]: converted in %.3fs", toc.secs - tic.secs + 1e-9 * (toc.nsecs - tic.nsecs))
+		tic = rospy.get_rostime()
                 GaBP_k.update_occ(occ)
                 toc = rospy.get_rostime()
-                rospy.loginfo("+++++ FACTOR GRAPH UPDATED in %.2fs +++++", toc.secs - tic.secs + 1e-9 * (toc.nsecs - tic.nsecs))
+                rospy.loginfo("+++++ FACTOR GRAPH UPDATED in %.3fs +++++", toc.secs - tic.secs + 1e-9 * (toc.nsecs - tic.nsecs))
                 mapLoop_start = rospy.get_time()
 
     except rospy.ROSInterruptException:
-        pass
+        GaBP_k.stop_thread = True
